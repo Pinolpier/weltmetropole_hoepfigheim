e9f1e607155af8119bbf0bdb4a8c9d0a3b173f35
AdmiralJo <11421371+AdmiralJo@users.noreply.github.com>
2020-09-11 05:59:01 +0200

    Höpfigheim-Wappen als Kompassnadel

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@
 
 import 'dart:async';
 import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:image/image.dart' as IMG;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_compass/flutter_compass.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -14,55 +18,26 @@ void main() {
 }
 
 class Compass extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-        title: 'Weltmetropole Höpfigheim',
-        theme: CupertinoThemeData(
-            brightness: Brightness.dark,
-            primaryColor: Colors.indigo,
-            barBackgroundColor: Colors.blueAccent,
-            scaffoldBackgroundColor: Colors.indigo),
-        home: CupertinoPageScaffold(
-          navigationBar:
-              CupertinoNavigationBar(middle: Text("Weltmetropole Höpfigheim")),
-          child: CompassWidget(),
-        ));
-    // return MaterialApp(
-    //   title: 'Flutter Demo',
-    //   theme: ThemeData(
-    //     // This is the theme of your application.
-    //     //
-    //     // Try running your application with "flutter run". You'll see the
-    //     // application has a blue toolbar. Then, without quitting the app, try
-    //     // changing the primarySwatch below to Colors.green and then invoke
-    //     // "hot reload" (press "r" in the console where you ran "flutter run",
-    //     // or simply save your changes to "hot reload" in a Flutter IDE).
-    //     // Notice that the counter didn't reset back to zero; the application
-    //     // is not restarted.
-    //     primarySwatch: Colors.blue,
-    //     // This makes the visual density adapt to the platform that you run
-    //     // the app on. For desktop platforms, the controls will be smaller and
-    //     // closer together (more dense) than on mobile platforms.
-    //     visualDensity: VisualDensity.adaptivePlatformDensity,
-    //   ),
-    //   home: CompassWidget(title: 'Flutter Demo Home Page'),
-    // );
+      title: 'Weltmetropole Höpfigheim',
+      theme: CupertinoThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.indigo,
+          barBackgroundColor: Colors.blueAccent,
+          scaffoldBackgroundColor: Colors.indigo),
+      home: CupertinoPageScaffold(
+        navigationBar:
+            CupertinoNavigationBar(middle: Text("Weltmetropole Höpfigheim")),
+        child: CompassWidget(),
+      ),
+    );
   }
 }
 
 class CompassWidget extends StatefulWidget {
   CompassWidget({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -71,6 +46,8 @@ class CompassWidget extends StatefulWidget {
 }
 
 class _CompassWidgetState extends State<CompassWidget> {
+  ui.Image _image;
+
   double _heading = 0;
   Position currentPosition;
 
@@ -81,6 +58,9 @@ class _CompassWidgetState extends State<CompassWidget> {
 
   void initState() {
     super.initState();
+
+    loadUiImage("assets/Wappen_Höpfigheim.png");
+
     FlutterCompass.events.listen(_onData);
     positionStream = getPositionStream(desiredAccuracy: LocationAccuracy.high)
         .listen((Position position) {
@@ -98,8 +78,11 @@ class _CompassWidgetState extends State<CompassWidget> {
           //     _getOffsetFromNorth(
           //         48.994719, 9.208169, 48.981298, 9.239052); //Test
           _heading = x -
-              _getOffsetFromNorth(currentPosition.latitude, currentPosition.longitude,
-                  48.981298, 9.239052); //Jonas Poolmitte
+              _getOffsetFromNorth(
+                  currentPosition.latitude,
+                  currentPosition.longitude,
+                  48.981298,
+                  9.239052); //Jonas Poolmitte
           // _heading = x -
           //     _getOffsetFromNorth(
           //         currentPosition.latitude,
@@ -125,6 +108,20 @@ class _CompassWidgetState extends State<CompassWidget> {
   final TextStyle _style =
       TextStyle(color: Colors.black, fontSize: 32, fontWeight: FontWeight.bold);
 
+  Future<ui.Image> loadUiImage(String imageAssetPath) async {
+    final ByteData data = await rootBundle.load(imageAssetPath);
+    final IMG.Image image = IMG.decodeImage(Uint8List.view(data.buffer));
+    final Completer<ui.Image> completer = Completer();
+    final IMG.Image resized = IMG.copyResize(image, width: 50);
+    final List<int> resizedBytes = IMG.encodePng(resized);
+    ui.decodeImageFromList(resizedBytes, (ui.Image img) {
+      _image = img;
+      return completer.complete(img);
+    });
+    return completer.future;
+  }
+
+
   Widget build(BuildContext context) {
     // return FutureBuilder(future: checkPermission(), builder: (BuildContext context, AsyncSnapshot<LocationPermission> snapshot) {
     //   if (snapshot.hasData) {
@@ -143,16 +140,50 @@ class _CompassWidgetState extends State<CompassWidget> {
     //   }
     //   return CupertinoActivityIndicator();
     // },);
-    return CustomPaint(
-        foregroundPainter: CompassPainter(angle: _heading),
-        child: Center(child: Text(_readout, style: _style)));
+
+    return _image == null
+        ? FutureBuilder<ui.Image>(
+            future: loadUiImage("assets/Nadel.png"),
+            // a Future<String> or null
+            builder: (BuildContext context, AsyncSnapshot<ui.Image> snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return new Text('Image loading...');
+                default:
+                  if (snapshot.hasError) {
+                    return CustomPaint(
+                      foregroundPainter: CompassPainter(angle: _heading, image: _image),
+                      child: Center(
+                        child: Text(_readout, style: _style),
+                      ),
+                    );
+                  } else {
+                    _image = snapshot.data;
+                    return CustomPaint(
+                      foregroundPainter:
+                          CompassPainter(angle: _heading, image: _image),
+                      child: Center(
+                        child: Text(_readout, style: _style),
+                      ),
+                    );
+                  }
+              }
+            },
+          )
+        : CustomPaint(
+            foregroundPainter: CompassPainter(angle: _heading, image: _image),
+            child: Center(
+              child: Text(_readout, style: _style),
+            ),
+          );
   }
 }
 
 class CompassPainter extends CustomPainter {
-  CompassPainter({@required this.angle}) : super();
+  CompassPainter({@required this.angle, @required this.image}) : super();
 
   final double angle;
+  final ui.Image image;
 
   double get rotation => -2 * pi * (angle / 360);
 
@@ -169,11 +200,17 @@ class CompassPainter extends CustomPainter {
     Offset center = Offset(size.width / 2, size.height / 2);
     Offset start = Offset.lerp(Offset(center.dx, radius), center, .4);
     Offset end = Offset.lerp(Offset(center.dx, radius), center, 0.1);
+    Offset imageOffset =
+        Offset.lerp(Offset(center.dx - 50, radius - 200), center, 0.5);
 
     canvas.translate(center.dx, center.dy);
     canvas.rotate(rotation);
     canvas.translate(-center.dx, -center.dy);
-    canvas.drawLine(start, end, needle);
+    if (image != null) {
+      canvas.drawImage(image, imageOffset, new Paint());
+    } else {
+      canvas.drawLine(start, end, needle);
+    }
     canvas.drawCircle(center, radius, circle);
   }
 
